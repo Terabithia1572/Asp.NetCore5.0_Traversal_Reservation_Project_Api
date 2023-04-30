b3b3bb03e87499bcafb1e207c304a1726353b097
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2023-04-30 15:28:24 +0300

    Add project files.

---
+++ Asp.NetCore5.0_Traversal_Reservation_Project_Api/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Asp.NetCore5._0_Traversal_Reservation_Project_Api
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
